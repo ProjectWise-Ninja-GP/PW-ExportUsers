98268f98aaa3c92b51fb55191ab2faa8b1472acb
ProjectWise-Ninja-GP <109803964+ProjectWise-Ninja-GP@users.noreply.github.com>
2022-08-12 16:40:47 +0200

    Add files via upload

---
+++ Export-Users.ps1
@@ -0,0 +1,14 @@
+﻿New-PWLogin -UseGui
+
+# Where would you like to save the csv files?
+$folderpath = "C:\Users\georgios.pavlidis\Documents\"
+
+# Which group name would you like to export? Make sure you include an asterisk if you want to export more than one group.
+$grps = get-PWGroups -GroupName "158454-2192*"
+
+foreach ($grp in $grps)
+{
+$outputfile = $folderpath+$grp.Name+".csv"
+Get-PWUsersInGroup -GroupName $grp.Name | Export-Csv $outputfile -Append
+
+}
